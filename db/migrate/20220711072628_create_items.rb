ce0454c766d50aa7946cf4e57dcd1851b1bc6654
t-mahito <qqna42p9@gmail.com>
2022-07-11 16:41:19 +0900

    itemモデルのマイグレーション記述
    active hash[genre]作成。

--- db/migrate/20220711072628_create_items.rb
+++ db/migrate/20220711072628_create_items.rb
@@ -2,6 +2,9 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
 
+      t.string  :name     ,null: false
+      t.text    :area     ,null: false
+      t.integer :genre_id ,null: false
       t.timestamps
     end
   end
