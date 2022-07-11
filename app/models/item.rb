99cfb1d4d75b003285925c89524796fda520918d
t-mahito <qqna42p9@gmail.com>
2022-07-11 16:46:16 +0900

    itemモデルのバリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :genre
+
+  validates :name, presence: true
+  validates :genre_id, numericality: { other_than: 0 , message: "ジャンルを選択してください。"}
 end
