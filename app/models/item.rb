f057ae0d53d024da35cbe0cd94aeb463ee774ae4
t-mahito <qqna42p9@gmail.com>
2022-07-12 14:44:49 +0900

    active storage導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :genre
+  has_one_attached :image
 
   validates :name, presence: true
   validates :genre_id, numericality: { other_than: 0 , message: "ジャンルを選択してください。"}
