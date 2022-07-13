22f54b7225e94139e6a723df21345159234cb9e6
t-mahito <qqna42p9@gmail.com>
2022-07-13 16:35:01 +0900

    item投稿フォームの作成
    データベース保存完了まで検証済み。
    保存後は投稿完了ページに遷移。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,20 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
-  
+
+  def create
+    @item = Item.new(item_params)
+
+    unless @item.save
+      render:new
+    end
+  end
+
+
+ private
+  def item_params
+    params.require(:item).permit(:name,:area,:genre_id,:image)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   
   root to: "items#index"
-  resources :items, only:[:index,:new]
+  resources :items, only:[:index,:new,:create]
 end
