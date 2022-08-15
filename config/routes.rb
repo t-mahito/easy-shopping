f9f3435b93f4512f6421705467b67566b7e7f66a
t-mahito <qqna42p9@gmail.com>
2022-08-15 15:26:28 +0900

    routeの記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#menu"
+  
   resources :items do
+   resources :comments, only: :create
+   
    collection do
     get "search"
-    get "list"
+    get "list"  
    end
+  
   end
 end
